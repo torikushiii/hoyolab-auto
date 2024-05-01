83136c23daeca245efada12b4d6b834df20be8ae
torikushiii <torikushiii@gmail.com>
2024-05-01 21:12:35 +0800

    Update retry limit and add hooks to handle rate limiting and error handling

--- singleton/got.js
+++ singleton/got.js
@@ -3,7 +3,7 @@ module.exports = (async () => {
 
 	const got = gotModule.default.extend({
 		retry: {
-			limit: 3
+			limit: 2
 		},
 		timeout: {
 			request: 5000
@@ -12,6 +12,35 @@ module.exports = (async () => {
 		throwHttpErrors: true,
 		headers: {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+		},
+		hooks: {
+			afterResponse: [
+				// just to avoid being rate limited if you have a lot of accounts
+				(response) => {
+					const accounts = app?.Account?.getActivePlatforms();
+					if (typeof accounts === "undefined") {
+						return response;
+					}
+
+					if (accounts.length >= 5) {
+						const delay = Math.floor(Math.random() * 2000) + 5000;
+						app.Logger.warn(`Delaying request for ${delay}ms`);
+						return new Promise((resolve) => setTimeout(() => resolve(response), delay));
+					}
+
+					return response;
+				}
+			],
+			beforeError: [
+				(e) => new app.Error.GenericRequest({
+					body: e.response?.body ?? null,
+					statusCode: e.response?.statusCode ?? null,
+					statusMessage: e.response?.statusMessage ?? null,
+					hostname: e.request?.options?.hostname,
+					message: e.message,
+					stack: e.stack
+				})
+			]
 		}
 	});
 
