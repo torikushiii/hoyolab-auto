f8edaeaff23310302231dbc3c4a04ff555bf67d3
torikushiii <torikushiii@gmail.com>
2024-06-20 18:34:57 +0800

    Update `MiHoYo` definitions

--- gots/mihoyo/index.js
+++ gots/mihoyo/index.js
@@ -1,7 +1,7 @@
 const definition = {
-	name: "Mihoyo",
-	optionsType: "function",
-	options: (() => ({
+	name: "MiHoYo",
+	optionsType: "object",
+	options: {
 		headers: {
 			Referer: "https://act.hoyolab.com",
 			"x-rpc-app_version": "1.5.0",
@@ -9,7 +9,7 @@ const definition = {
 			"x-rpc-language": "en-us",
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
 		}
-	})),
+	},
 	parent: "Global"
 };
 
