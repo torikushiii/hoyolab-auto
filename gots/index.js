7cb20dbe6097f46eeb7c58cd19c7176035d0be6e
torikushiii <torikushiii@gmail.com>
2024-06-20 18:33:52 +0800

    Added `MiHoYo`

--- gots/index.js
+++ gots/index.js
@@ -2,12 +2,14 @@ const API = require("./api/index.js");
 const FakeAgent = require("./fake-agent/index.js");
 const Global = require("./global/index.js");
 const HoyoLab = require("./hoyolab/index.js");
+const MiHoYo = require("./mihoyo/index.js");
 
 const definitions = [
 	API,
 	FakeAgent,
 	Global,
-	HoyoLab
+	HoyoLab,
+	MiHoYo
 ];
 
 module.exports = definitions;
