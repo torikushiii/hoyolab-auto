1ef7ad6234e7d8868bcdf3e49d62b63266a39c08
torikushiii <torikushiii@gmail.com>
2024-06-24 05:10:09 +0800

    Implemented Genshin Code Redemption
    - Implemented Genshin Impact code redemption functionality in cron job.
    - Added code resolvers to fetch codes from various sources (Game8, Genshin Fandom, HoyoLab).
    - Implemented caching for Genshin Impact codes to avoid redundant requests.
    - Added logging and notifications for successful and failed code redemptions.

--- crons/code-redeem/index.js
+++ crons/code-redeem/index.js
@@ -22,9 +22,78 @@ module.exports = {
 			return;
 		}
 
-		// not yet implemented
 		if (genshin.length !== 0) {
-			return;
+			const accounts = app.HoyoLab.getActiveAccounts({ whitelist: "genshin" });
+			if (accounts.length === 0) {
+				app.Logger.debug("CodeRedeem", "No active accounts found for Genshin Impact.");
+				return;
+			}
+
+			const telegram = app.Platform.get(2);
+			const webhook = app.Platform.get(3);
+			for (const account of accounts) {
+				if (account.redeemCode === false) {
+					continue;
+				}
+
+				const { success, failed } = await CodeResolver.redeemGenshin(account, genshin);
+				if (success.length !== 0) {
+					for (const code of success) {
+						const embed = {
+							color: 0xBB0BB5,
+							title: "Genshin Impact Code Redeem",
+							description: `(${account.uid}) ${account.nickname}`
+							+ `\nCode Successfully Redeemed!`
+							+ `\nCode: \`${code.code}\``
+							+ `\nRewards: ${code.rewards.join(", ")}`,
+							timestamp: new Date()
+						};
+
+						if (webhook) {
+							await webhook.send(embed);
+						}
+						if (telegram) {
+							const message = [
+								`[Genshin Impact] (${account.uid}) ${account.nickname}`,
+								`Code Successfully Redeemed!`,
+								`Code: ${code.code}`,
+								`Rewards: ${code.rewards.join(", ")}`
+							].join("\n");
+
+							const escapedMessage = app.Utils.escapeCharacters(message);
+							await telegram.send(escapedMessage);
+						}
+					}
+				}
+
+				if (failed.length !== 0) {
+					for (const code of failed) {
+						const embed = {
+							color: 0xBB0BB5,
+							title: "Genshin Impact Code Redeem",
+							description: `(${account.uid}) ${account.nickname}`
+							+ `\nFailed to redeem code: \`${code.code}\``
+							+ `\nManually Redeem Here:\nhttps://genshin.hoyoverse.com/en/gift?code=${code.code}`,
+							timestamp: new Date()
+						};
+
+						if (webhook) {
+							await webhook.send(embed);
+						}
+						if (telegram) {
+							const message = [
+								`[Genshin Impact] (${account.uid}) ${account.nickname}`,
+								`Code Failed to Redeem!`,
+								`Code: ${code.code}`,
+								`Manually Redeem Here: https://genshin.hoyoverse.com/en/gift?code=${code.code}`
+							].join("\n");
+
+							const escapedMessage = app.Utils.escapeCharacters(message);
+							await telegram.send(escapedMessage);
+						}
+					}
+				}
+			}
 		}
 
 		if (starrail.length !== 0) {
--- crons/code-redeem/resolvers/genshin/index.js
+++ crons/code-redeem/resolvers/genshin/index.js
@@ -1,4 +1,56 @@
-const fetchAll = async () => [];
+const Defaults = require("./defaults");
+const HoyoLab = require("./hoyolab");
+
+const fetchAll = async () => {
+	const [defaults, hoyolab] = await Promise.all([
+		Defaults.fetch(),
+		HoyoLab.fetch()
+	]);
+
+	const cachedCodes = await app.Cache.get("genshin-code") || [];
+	const codes = new Set(cachedCodes.map(i => i.code));
+
+	const data = [...defaults, ...hoyolab]
+		.filter(i => i.code !== "")
+		.filter(i => i.rewards.length !== 0)
+		.filter((i, index, self) => self.findIndex(t => t.code === i.code) === index)
+		.filter(Boolean);
+
+	app.Logger.debug("GenshinResolver", `Fetched ${data.length} codes`);
+
+	if (cachedCodes.length === 0) {
+		app.Logger.warn("GenshinResolver", "No cached codes found, caching all codes");
+		await app.Cache.set({
+			key: "genshin-code",
+			value: data.map(i => ({
+				...i,
+				date: Date.now(),
+				active: false
+			}))
+		});
+
+		return [];
+	}
+
+	const filteredData = data.filter(i => !codes.has(i.code));
+	if (filteredData.length === 0) {
+		app.Logger.debug("GenshinResolver", "No new codes found");
+		return [];
+	}
+
+	app.Logger.info("GenshinResolver", `Found ${filteredData.length} new codes`);
+
+	const newCodes = filteredData.map(i => ({ ...i, date: Date.now(), active: false }));
+	await app.Cache.set({
+		key: "genshin-code",
+		value: [
+			...cachedCodes,
+			...newCodes
+		]
+	});
+
+	return newCodes;
+};
 
 module.exports = {
 	fetchAll
