0d600de1a59e2a078054029c7f20ae172360b445
torikushiii <torikushiii@gmail.com>
2024-07-12 03:56:32 +0800

    return `zenless` instead of nap in platform list

--- hoyolab-modules/template.js
+++ hoyolab-modules/template.js
@@ -456,7 +456,7 @@ module.exports = class HoyoLab {
 				return null;
 			}
 
-			return i.name;
+			return (i.name === "nap") ? "zenless" : i.name;
 		}).filter(i => i !== null);
 
 		return platforms;
