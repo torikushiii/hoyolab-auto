e8c50cfe06ca628bdbf32645056caa894a5a6572
Abigail de Joode <contact@abbydiode.com>
2024-07-18 21:11:26 +0200

    Update convert.js to include kebab-case to camelCase conversion

--- convert.js
+++ convert.js
@@ -1,9 +1,25 @@
-const file = require("fs");
+const file = require("node:fs");
 const JSON5 = require("json5");
 
 const config = require("./config.js");
 
-const jsonString = JSON5.stringify(config, null, 4);
+function kebabToCamelCase(object) {
+    Object.keys(object).forEach((key) => {
+        if (object[key] && typeof object[key] === "object" && !Array.isArray(object[key])) {   
+            kebabToCamelCase(object[key]);
+        }
+        
+        const convertedKey = key.replace(/-./g, match => match[1].toUpperCase());
+        
+        if (key !== convertedKey) {
+            object[convertedKey] = object[key];
+            delete object[key];
+        }
+    });
 
-file.writeFileSync("./config.json5", jsonString);
-console.log("Config file converted to JSON5!");
+}
+
+kebabToCamelCase(config);
+
+file.writeFileSync("./config.json5", JSON5.stringify(config, null, 4));
+console.log("config.js converted to config.json5");
