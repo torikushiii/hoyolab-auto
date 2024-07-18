2b8617e6740a40ee2511f2c6706b3c821193d499
torikushiii <torikushiii@gmail.com>
2024-07-19 05:08:46 +0800

    linted

--- convert.js
+++ convert.js
@@ -3,19 +3,19 @@ const JSON5 = require("json5");
 
 const config = require("./config.js");
 
-function kebabToCamelCase(object) {
-    Object.keys(object).forEach((key) => {
-        if (object[key] && typeof object[key] === "object" && !Array.isArray(object[key])) {
-            kebabToCamelCase(object[key]);
-        }
+function kebabToCamelCase (object) {
+	for (const key of Object.keys(object)) {
+		if (object[key] && typeof object[key] === "object" && !Array.isArray(object[key])) {
+			kebabToCamelCase(object[key]);
+		}
 
-        const convertedKey = key.replace(/-./g, match => match[1].toUpperCase());
+		const convertedKey = key.replace(/-./g, match => match[1].toUpperCase());
 
-        if (key !== convertedKey) {
-            object[convertedKey] = object[key];
-            delete object[key];
-        }
-    });
+		if (key !== convertedKey) {
+			object[convertedKey] = object[key];
+			delete object[key];
+		}
+	}
 }
 
 kebabToCamelCase(config);
