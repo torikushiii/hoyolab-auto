c6d0e717616f66e3b34cd934cab4818708ec14c7
torikushiii <torikushiii@gmail.com>
2024-07-25 00:02:31 +0800

    Remove deprecated MiHoYo got instance

--- gots/index.js
+++ gots/index.js
@@ -3,15 +3,13 @@ const FakeAgent = require("./fake-agent/index.js");
 const Global = require("./global/index.js");
 const HoyoClient = require("./hoyo-client/index.js");
 const HoyoLab = require("./hoyolab/index.js");
-const MiHoYo = require("./mihoyo/index.js");
 
 const definitions = [
 	API,
 	FakeAgent,
 	Global,
 	HoyoClient,
-	HoyoLab,
-	MiHoYo
+	HoyoLab
 ];
 
 module.exports = definitions;
