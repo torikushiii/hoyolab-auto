6be2af43b76ab5e62aa9967028e019579e1e8fa9
torikushiii <torikushiii@gmail.com>
2024-08-17 04:52:46 +0800

    removed redundant headers

--- hoyolab-modules/genshin/diary.js
+++ hoyolab-modules/genshin/diary.js
@@ -152,11 +152,6 @@ module.exports = class Diary {
 					lang: "en-us"
 				},
 				headers: {
-					Referer: "https://act.hoyolab.com",
-					"x-rpc-app_version": "1.5.0",
-					"x-rpc-client_type": 5,
-					"x-rpc-language": "en-us",
-					"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
 					Cookie: accountData.cookie,
 					DS: app.Utils.generateDS()
 				}
