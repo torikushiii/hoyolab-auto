698b54aeb54535a0f8930f76cf559d62dd786e1e
torikushiii <torikushiii@gmail.com>
2024-08-26 05:12:37 +0800

    reverted `missed-check-in` to original implementation

--- crons/missed-check-in/index.js
+++ crons/missed-check-in/index.js
@@ -1,15 +1,18 @@
-const RegionalTaskManager = new app.RegionalTaskManager();
-
-RegionalTaskManager.registerTask("MissedCheckIn", 23, 0, async (account) => {
-	const platform = app.HoyoLab.get(account.platform);
-	await platform.checkIn(account);
-});
-
 module.exports = {
 	name: "missed-check-in",
-	expression: "*/5 * * * *",
+	expression: "0 0 23 * * *",
 	description: "This check if all accounts successfully checked in today before daily reset",
 	code: (async function missedCheckIn () {
-		await RegionalTaskManager.executeTasks();
+		const accounts = app.HoyoLab.getActiveAccounts();
+		if (accounts.length === 0) {
+			app.Logger.warn("Cron:MissedCheckIn", "No active accounts found for HoyoLab");
+			return;
+		}
+
+		const activeGameAccounts = app.HoyoLab.getActivePlatform();
+		for (const name of activeGameAccounts) {
+			const platform = app.HoyoLab.get(name);
+			await platform.checkIn();
+		}
 	})
 };
