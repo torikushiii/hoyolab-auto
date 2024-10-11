06a6ad20f1b2786a9bd48d3864737174905604ff
torikushiii <torikushiii@gmail.com>
2024-10-11 20:04:55 +0800

    updated `user-agent`

--- gots/fake-agent/index.js
+++ gots/fake-agent/index.js
@@ -3,7 +3,7 @@ const definition = {
 	optionsType: "object",
 	options: {
 		headers: {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 		}
 	},
 	parent: "Global"
